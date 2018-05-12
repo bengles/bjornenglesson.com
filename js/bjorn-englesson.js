1adedac0724f1d61b872058f10236a9be2f99499
Björn Englesson <engles@kth.se>
2018-05-12 15:56:51 +0200

    Trying to fix background

--- js/bjorn-englesson.js
+++ js/bjorn-englesson.js
@@ -1,4 +1,12 @@
 $(function ()
 {
 	$("a").attr('target', '_blank');
+
+	if ($(window).width() < 1080) {
+		$(document).scroll(function() 
+		{
+			$('body').css('background-position', '0px ' + $(document).scrollTop() + 'px');
+		});
+	}
+
 });
