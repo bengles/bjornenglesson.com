aa73732c7de3952f22cb4019b6d066803d5ccbce
Björn Englesson <engles@kth.se>
2018-05-12 16:07:13 +0200

    Added backstretch

--- js/bjorn-englesson.js
+++ js/bjorn-englesson.js
@@ -2,11 +2,6 @@ $(function ()
 {
 	$("a").attr('target', '_blank');
 
-	if ($(window).width() < 1080) {
-		$(document).scroll(function() 
-		{
-			$('body').css('background-position', '0px ' + $(document).scrollTop() + 'px');
-		});
-	}
+	 $.backstretch("img/background-2.png");
 
 });
